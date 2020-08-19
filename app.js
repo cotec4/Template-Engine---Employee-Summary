dc6b513bbc6deb15040ca7d66068b210053b2ff2
Connor Cote <cotec4@gmail.com>
2020-08-19 00:30:35 -0400

    initial commit, code is running but prompt is jumping past manager questions and immediately going to choice selection

--- app.js
+++ app.js
@@ -9,8 +9,117 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+let employeeList = [];
 
+userChoice();
 
+function userChoice() {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is your name?"
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is your ID?"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "What is your email?"
+        },
+        {
+            name: "officeNumber",
+            type: "input",
+            message: "What is your office number?"
+        }]).then(
+            inquirer.prompt([
+                {
+                    name: "choice",
+                    type: "list",
+                    message: "Do you want to add a new employee?",
+                    choices: ["Intern", "Engineer", "No"]
+                }
+            ])).then((answer) => {
+                switch (answer.choice) {
+                    case "Intern":
+                        return createIntern();
+                    case "Engineer":
+                        return createEngineer();
+                    case "No":
+                        return htmlRender();
+                    default:
+                        break;
+                };
+            });
+};
+
+function createIntern() {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is your intern's name?"
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is your intern's ID?"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "What is your intern's email?"
+        },
+        {
+            name: "school",
+            type: "input",
+            message: "What university does your intern attend?"
+        }
+    ]).then((answer) => {
+        const intern = new Intern(answer.name, answer.id, answer.email, answer.school);
+        employeeList.push(intern);
+        userChoice();
+    });
+};
+
+function createEngineer() {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is your engineer's name?"
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is your engineer's ID?"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "What is your engineer's email?"
+        },
+        {
+            name: "github",
+            type: "input",
+            message: "What is your engineer's Github username?"
+        }
+    ]).then((answer) => {
+        const engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+        employeeList.push(engineer);
+        userChoice();
+    });
+};
+
+function htmlRender() {
+    const html = render(employeeList);
+    fs.writeFileSync(outputPath, html);
+};
+
+// returned from the `render` function. Now write it to a file named `team.html` in the
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -19,7 +128,6 @@ const render = require("./lib/htmlRenderer");
 // generate and return a block of HTML including templated divs for each employee!
 
 // After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
